223627b2febcf2d8e9d4f80eb519b14ff65e1f2c
Danielle Brown <hello@danielleallyssa.com>
2024-05-15 01:46:28 -0400

    Adding game logic

--- script.js
+++ script.js
@@ -10,11 +10,31 @@ const resetButton = document.querySelector("#reset");
 // Sets the initial value of the player input
 let currentPlayer = "X";
 
+// Winning conditions
+const winConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+for (let i = 0; i < winConditions.length; i++) {
+  for (let j = 0; j < 3; j++) {
+    console.log(winConditions[i][j]);
+  }
+}
+
 // EVENT LISTENER
 cells.forEach((cell) =>
   cell.addEventListener("click", function () {
-    changePlayer();
-    changeCellContent(cell);
+    if (cell.textContent === "") {
+      changePlayer();
+      changeCellContent(cell);
+    }
   })
 );
 

92f6d6f979b6e39c704b4d7e314098f3469071fd
Danielle Brown <hello@danielleallyssa.com>
2024-05-15 00:03:12 -0400

    Cleaned up code for the group

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict";
 
-// Selects cells as a node list
+// VARIABLES
+// Selects cells as a node list/array
 const cells = document.querySelectorAll(".cell");
 
 // Selects button as a reset
@@ -9,9 +10,9 @@ const resetButton = document.querySelector("#reset");
 // Sets the initial value of the player input
 let currentPlayer = "X";
 
+// EVENT LISTENERS
 cells.forEach((cell) =>
   cell.addEventListener("click", function () {
-    // Alternates between X and O
     changePlayer();
     changeCellContent(cell);
   })
@@ -21,6 +22,8 @@ resetButton.addEventListener("click", function () {
   cells.forEach((cell) => clearCellContent(cell));
 });
 
+// FUNCTIONS
+// Alternates between X and O
 const changePlayer = () => {
   currentPlayer = currentPlayer === "X" ? "O" : "X";
 };

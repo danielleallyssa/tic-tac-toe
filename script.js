baf00710b9351c2ade7eff4fb8314eedcefd2d42
Danielle Brown <hello@danielleallyssa.com>
2024-05-16 19:58:58 -0400

    Organized the code into sections and also added changePlayer to the if statement so the player doesn't change on click

--- script.js
+++ script.js
@@ -10,18 +10,35 @@ const resetButton = document.querySelector("#reset");
 // Sets the initial value of the player input
 let currentPlayer = "X";
 
+// Define win conditons
+const winConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+// Board Array
+let boardArray = [
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
+];
+
 // EVENT LISTENER
 cells.forEach((cell, index) =>
   cell.addEventListener("click", function () {
-    if(cell.textContent === ''){
-      changeCellContent(cell)
+    if (cell.textContent === "") {
+      changeCellContent(cell);
+      changePlayer();
     }
-    changePlayer();
-<<<<<<< main
+
     changeCellContent(cell);
-    updateBoardArray(cell, index)
-=======
->>>>>>> main
+    updateBoardArray(cell, index);
   })
 );
 
@@ -47,28 +64,8 @@ const clearCellContent = (element) => {
   element.style.color = "black";
 };
 
-// CHECK WINNER
-// Define win conditons
-const winConditions = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-]
-
-// Board Array
-let boardArray = [
-  ["", "", ""],
-  ["", "", ""],
-  ["", "", ""]
-]
-
 // Update board array with plays
-function updateBoardArray(cell, index)  {
+function updateBoardArray(cell, index) {
   boardArray[index] = currentPlayer;
   console.log(boardArray);
 }
